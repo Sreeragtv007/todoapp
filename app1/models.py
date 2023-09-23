231106321d44f4e39cb924abc4f7cf2607ecc2f5
sreerag <sreeragtv007@gmail.com>
2023-09-23 10:12:58 +0530

    todo time updated sucessfully

--- app1/models.py
+++ app1/models.py
@@ -3,4 +3,12 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     name=models.CharField(max_length=50)
+    date=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering=["-date"]
+
+       
+
+   
     
